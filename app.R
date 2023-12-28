379df3c3ac69948057dbb52f8b78646892d7f80b
Aravindh <Ktaravindh005@gmail.com>
2023-12-27 20:48:47 -0600

    Worked on the Visualization and Machine Learning Implementation

--- app.R
+++ app.R
@@ -3,6 +3,17 @@
 library(shiny)
 library(shinythemes)
 library(RMySQL)
+library(data.table)
+library(RCurl)
+library(randomForest)
+
+data(airquality)
+
+# Read data
+weather <- read.csv(text = getURL("https://raw.githubusercontent.com/dataprofessor/data/master/weather-weka.csv"),  stringsAsFactors = TRUE)
+
+# Build model
+model <- randomForest(play ~ ., data = weather, ntree = 500, mtry = 4, importance = TRUE)
 
 rm(list = ls())
 readRenviron(".env")
@@ -66,7 +77,7 @@ readRenviron(".env")
   }
 
   # Define UI
-  ui <- fluidPage(theme = shinytheme("cerulean"),
+  ui <- fluidPage(theme = shinytheme("cyborg"),
     navbarPage(
 
       "My Shiny App",
@@ -97,15 +108,92 @@ readRenviron(".env")
                             
                )
       ),
-      tabPanel("Graphs", "This panel is intentionally left blank."),
-      tabPanel("Machine Learning", "This panel is intentionally left blank")
+      tabPanel("Visualization",
+               sidebarLayout(
+                sidebarPanel(
+                  sliderInput(
+                       inputId = "bins",
+                       label = "Number of bins : ",
+                       min = 1,
+                       max = 50,
+                       value = 30)
+                  ),
+                   
+                mainPanel(
+                     
+                     h1("Histogram"),
   
+                     # Output - Histogram
+                     plotOutput(outputId = "distPlot")
+                   )
+                   
+               )),
+               
+      tabPanel("Machine Learning", 
+        
+               headerPanel('Play Golf?'),
+               
+               # Input values
+               sidebarPanel(
+                 HTML("<h3>Input parameters</h3>"),
+                 
+                 selectInput("outlook", label = "Outlook:", 
+                             choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
+                             selected = "Rainy"),
+                 sliderInput("temperature", "Temperature:",
+                             min = 64, max = 86,
+                             value = 70),
+                 sliderInput("humidity", "Humidity:",
+                             min = 65, max = 96,
+                             value = 90),
+                 selectInput("windy", label = "Windy:", 
+                             choices = list("Yes" = "TRUE", "No" = "FALSE"), 
+                             selected = "TRUE"),
+                 
+                 actionButton("submitbutton", "Submit", class = "btn btn-primary")
+               ),
+               
+               mainPanel(
+                 tags$label(h3('Status/Output')), # Status/Output Text Box
+                 verbatimTextOutput('contents'),
+                 tableOutput('tabledata') # Prediction results table
+                 
+               ))
     ) # navbarPage
   ) # fluidPage
 
-  
   # Define server function  
-  server <- function(input, output) {
+  server <- function(input, output, session) {
+    
+    # Input Data
+    datasetInput <- reactive({  
+      
+      # outlook,temperature,humidity,windy,play
+      df <- data.frame(
+        Name = c("outlook",
+                 "temperature",
+                 "humidity",
+                 "windy"),
+        Value = as.character(c(input$outlook,
+                               input$temperature,
+                               input$humidity,
+                               input$windy)),
+        stringsAsFactors = FALSE)
+      
+      play <- "play"
+      df <- rbind(df, play)
+      input <- transpose(df)
+      write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+      
+      test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
+      
+      test$outlook <- factor(test$outlook, levels = c("overcast", "rainy", "sunny"))
+      
+      
+      Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
+      print(Output)
+      
+    })
     
     output$txtout <- renderText({
       paste( input$txt1, input$txt2, sep = " " )
@@ -115,8 +203,32 @@ readRenviron(".env")
       return(get_user_details(input$EmailID))
     })
     
-  } # server
-  
+    output$distPlot <- renderPlot({
+      
+      x <- airquality$Ozone
+      x <- na.omit(x)
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      hist(x, breaks = bins, col = "#11AADB", border = "Black",
+           xlab = "ozone level",
+           main = "Histogram of Ozone level")
+    
+    output$contents <- renderPrint({
+      if (input$submitbutton>0) {
+        isolate("Calculation Complete")
+      } else {
+        return("Server is ready for calculation")
+      }
+    })
+    
+    output$tabledata <- renderTable({
+      if (input$submitbutton>0) {
+        isolate(datasetInput())
+      }
+    })
+    
+  }) # server
+}
 
   # Create Shiny object
   shinyApp(ui = ui, server = server)
